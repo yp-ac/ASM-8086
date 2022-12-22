234cb2ea9568ef152956cccd4e075d02f21c55d5
Yash Pawar <yashpawarp@gmail.com>
2022-12-22 18:32:23 +0530

    32 bit addition

--- src/add-sub/32-add.asm
+++ src/add-sub/32-add.asm
@@ -1,17 +1,34 @@
+; Addition of 32 bit numbers
 .MODEL SMALL
 .DATA 
-    NUM1 DB 17h
-    NUM2 DB 12h
+    NUM1 DD 12344417h
+    NUM2 DD 1432E342h 
+    SUM  DD ?
     
 .CODE
     MAIN PROC
         MOV AX, @DATA
         MOV DS, AX
         
-        MOV Bl, NUM1
-        MOV Dl, NUM2
-        ADD Bl, Dl    ; ADDITION
+        MOV BX, NUM1      ; LOAD LSB      
+        LEA SI, NUM1
+        MOV AX, [SI + 02] ; LOAD MSB 
+        
+        MOV DX, NUM2      ; LOAD LSB
+        LEA SI, NUM2
+        MOV CX, [SI + 02] ; LOAD MSB
+        
+        ADC BX, DX
+        JNC CONTINUE      ; Jump Not Carry
+        INC AX  
+        
+        CONTINUE:            
+            ADC AX, CX
+        
+        MOV SUM, BX       ; STORE LSB of result
+        LEA SI, SUM
+        MOV [SI + 02], AX ; STORE MSB of result
         
         MOV AH, 4Ch
-        INT 21h
-    MAIN ENDP 
+        INT 21h                                
+    MAIN ENDP  
